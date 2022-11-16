6e2f76615361e3f547b439b9777f7f10f02f501f
Hassan <h.shahzad@zweidevs.com>
2022-11-16 16:07:27 +0500

    regester model on admin site

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models  import User
+class UserAdminDisplay(admin.ModelAdmin):
+    
+    def firstname_and_lastname(obj,obj1):
+     return "%s %s" % (obj1.first_name, obj1.last_name)
+    
+   
+    list_display=['__str__','firstname_and_lastname','email','school','dob','profile_image']
+    
 
-# Register your models here.
+    def get_exclude(self, request, obj=None):
+     if obj:
+        return ["password"]
+     else:
+        return []
+
+
+    
+
+
+
+admin.site.register(User,UserAdminDisplay)
