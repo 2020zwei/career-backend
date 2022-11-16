458b3e889b53e441cb1cd7a67557c75d83578773
Hassan <h.shahzad@zweidevs.com>
2022-11-16 16:31:56 +0500

    user model create basic fields

--- user/models.py
+++ user/models.py
@@ -1,3 +1,28 @@
+from .validators import validate_file_size
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    """Model to create User"""
+    email = models.EmailField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    school = models.CharField(max_length=50, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images',null=True,blank=True,validators=[validate_file_size])  
+    dob = models.DateField()
+
+    username = None
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+
+    
+
+    def __str__(self):
+        """return email of User"""
+        return self.email
+    
+
