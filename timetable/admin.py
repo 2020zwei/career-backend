f01e69d049bad68f6c8cf56b0b4b9734555646d4
Hassan <h.shahzad@zweidevs.com>
2022-11-18 18:48:37 +0500

    added list_display

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Slot
+class TimeSlotAdminDisplay(admin.ModelAdmin):
+    
+    
+    
+   
+    list_display=['id','user']
+admin.site.register(Slot,TimeSlotAdminDisplay)
