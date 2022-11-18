9039917b60c2413468def02dc2929c3a693dba20
Hassan <h.shahzad@zweidevs.com>
2022-11-18 18:51:09 +0500

    created model od SLot

--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from user.models import Student
+from .choices import DAY_OF_THE_WEEK
 
-# Create your models here.
+
+
+
+
+
+
+class Slot(models.Model):
+    """Model to create TimeTable"""
+    
+    timeslot = models.CharField(max_length=50)
+    day = models.CharField(choices=DAY_OF_THE_WEEK.choices,max_length=1)
+    year = models.PositiveSmallIntegerField()
+    week = models.PositiveSmallIntegerField()
+    user=models.ForeignKey(Student,on_delete=models.CASCADE,related_name='Student')
+
+
+
+    def __str__(self):
+        
+        return (self.user.first_name +" " + self.user.last_name)
+
+    
+
+
+    
+
