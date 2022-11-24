597a747df21a78e095e2ce83b8ed802e3b80ebdd
Hassan <h.shahzad@zweidevs.com>
2022-11-24 18:36:01 +0500

    added choices for result and level

---
+++ cv/choices.py
@@ -0,0 +1,21 @@
+from django.utils.translation import gettext as _
+from django.db import models
+
+
+class JUNIOR_CERT_TEST_LEVEL(models.TextChoices):
+
+    Common = "1", _('Common')
+    Higher = "2", _('Higher')
+    Ordinary = "3", _('Ordinary')
+
+class JUNIOR_CERT_TEST_RESULT(models.TextChoices):
+
+    HIGHERMERIT = "1", _('HIGHER MERIT')
+    MERIT = "2", _('MERIT')
+    ACHIEVED = "3", _('ACHIEVED')
+    PARTIALLYACHIEVED = "4", _('PARTIALLY ACHIEVED')
+    NOTGRADED= "5", _('NOT GRADED')
+    
+    
+
+
