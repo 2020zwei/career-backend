7c31acdf6c477ee3fc5b2d0ee9b6eb8498f1f83f
Hassan <h.shahzad@zweidevs.com>
2022-11-24 18:13:23 +0500

    added new fields in model

--- user/models.py
+++ user/models.py
@@ -15,6 +15,10 @@ class Student(models.Model):
     profile_image = models.ImageField(upload_to='profile_images',null=True,blank=True,validators=[validate_file_size])  
     dob = models.DateField(null=True,blank=True)
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='student')
+    city = models.CharField(max_length=50, blank=True,null=True)
+    country = models.CharField(max_length=50, blank=True,null=True)
+    address = models.TextField(blank=True,null=True)
+    eircode=models.CharField(max_length=7,null=True,blank=True)
 
 
 
