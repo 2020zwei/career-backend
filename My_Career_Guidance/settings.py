7d7ecf0c88612bc2e72e1f60b36903e3233556e9
Hassan <h.shahzad@zweidevs.com>
2022-11-24 18:34:00 +0500

    added postgres database

--- My_Career_Guidance/settings.py
+++ My_Career_Guidance/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'user',
     'timetable',
     'calculator',
+    'cv',
     
 ]
 MIDDLEWARE = [
@@ -97,10 +98,20 @@ WSGI_APPLICATION = 'My_Career_Guidance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'MyCareerGuidance',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
