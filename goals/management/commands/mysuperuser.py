13ed0787f0f0172abdfd93b6d5d988a10f90e46b
talootkhan <talootkhan09@gmail.com>
2023-04-10 11:43:21 +0500

    superuser fixed

--- goals/management/commands/mysuperuser.py
+++ goals/management/commands/mysuperuser.py
@@ -5,6 +5,5 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin',
-                                          'admin@gmail.com',
+            User.objects.create_superuser('admin@gmail.com',
                                           'admin@123')
