d6c211b4ac18669ff1427db1e5d7846ab11e9fac
Hussnain <hussnain.shabbir@zweidevs.com>
2023-04-27 16:52:53 +0500

    Create API for get all Psycometric Tests

--- psychometric/serializers.py
+++ psychometric/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import PsychometricTest, TestType,Question,Answer  
+from .models import PsychometricTest, TestType, Question, Answer, TestResult, TestResultDetail
 from users.models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -26,3 +26,46 @@ class PsychometricTestSerializer(serializers.ModelSerializer):
     class Meta:
         model=PsychometricTest
         fields=['name','question']
+
+
+class PsychometricResultDetailSerializer(serializers.ModelSerializer):
+    result = serializers.IntegerField(required=False)
+    question = QuestionSerializer(read_only=True)
+    answer = AnswerSerializer(read_only=True)
+
+    class Meta:
+        model = TestResultDetail
+        fields = ['result', 'question', 'answer']
+
+
+class PsychometricStatusSerializer(serializers.ModelSerializer):
+    complete = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
+    total_score = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PsychometricTest
+        fields = ['id', 'name', 'complete', 'score', 'total_score']
+
+    def get_complete(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            # Check if the user has completed the quiz
+            result = TestResult.objects.filter(user__user__email=request.user.email, test=obj).last()
+            if result:
+                return True
+        return False
+
+    def get_score(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            # Get the user's score for the quiz
+            result = TestResult.objects.filter(user__user__email=request.user.email, test=obj).last()
+            if result:
+                return result.score
+        return None
+
+    def get_total_score(self, obj):
+        questions = obj.question.all()
+        total_score = sum([max(question.answer.all(), key=lambda x: x.weightage).weightage for question in questions])
+        return total_score
