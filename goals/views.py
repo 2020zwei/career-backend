0628015b3efa055ba2d6f58a68663a67b0658ca5
Hussnain <hussnain.shabbir@zweidevs.com>
2023-05-02 18:42:34 +0500

    setting date2

--- goals/views.py
+++ goals/views.py
@@ -80,7 +80,10 @@ class GoalViewRelated2(CreateAPIView):
           proffession=request.data.get('proffession')
           goal=request.data.get('goal')
           realistic=request.data.get('realistic')
-          goal_obj=Goal.objects.create(user=user_obj.student,proffession=proffession, goal=goal,realistic=realistic)
+          countdown_str = request.data.get('date')
+          countdown = datetime.strptime(countdown_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+          print(countdown)
+          goal_obj=Goal.objects.create(user_id=user_obj.id,proffession=proffession, goal=goal,realistic=realistic, countdown=countdown)
           goal_obj.save()
 
           return Response(data={'success': True, 'Goals': goal_obj.goal}, status=status.HTTP_200_OK)
