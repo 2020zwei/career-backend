ca928a64d5e989137060bee3b54bf2f40b0415dd
talootkhan <talootkhan09@gmail.com>
2023-07-11 20:16:57 +0500

    add quality serializer save

--- cv/views.py
+++ cv/views.py
@@ -256,8 +256,8 @@ class SkillsViewRelated(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         try:
-            skill_serializer_obj=SkillSerializer(instance='',data=request.data,many=True, context=request)
-            quality_serializer_obj=QualitiesSerializer(instance='',data=request.data.get('quality_data'),many=True, context=request)
+            skill_serializer_obj=SkillSerializer(instance='',data=request.data, context=request)
+            quality_serializer_obj=QualitiesSerializer(instance='',data=request.data.get('quality_data'), context=request)
 
             if skill_serializer_obj.is_valid(raise_exception=True):
                     if quality_serializer_obj.is_valid(raise_exception=True):
