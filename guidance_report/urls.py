6b37dda03b0de685d4e355b569075c54c8bd3deb
warismstf <warismstf@gmail.com>
2024-11-13 13:19:50 +0500

    Guideline Report

--- guidance_report/urls.py
+++ guidance_report/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import GenerateGpt, GeneratePDFReport, GuidanceReportModels, ChatbotAPIView
+from .views import GenerateGpt, GeneratePDFReport, GuidanceReportModels, GenerateGuidanceReportGPT
 
 
 urlpatterns = [
     path("get-report-models/", GuidanceReportModels.as_view(), name="report-models"),
     path('my-guidance-pdf-report/', GeneratePDFReport.as_view(), name='my-guidance-pdf-report'),
     path('generate-report/', GenerateGpt.as_view(), name='generate-report-response'),
-    path('chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
+    path('generate-guidance-report/', GenerateGuidanceReportGPT.as_view(), name='generate-guidance-report-response'),
 ]
